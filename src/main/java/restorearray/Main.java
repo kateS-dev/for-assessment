f69fe84b14eed4af04ee096595af6113155b9b95
Kate <192233029+kateS-dev@users.noreply.github.com>
2026-02-24 16:59:26 -0500

    chore: adding frame here

---
+++ src/main/java/restorearray/Main.java
@@ -0,0 +1,10 @@
+package restorearray;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] test = {{2, 1}, {3, 4}, {3, 2}};
+        int[] res = solution.restoreArray(test);
+        System.out.println("res dddddd: " +res.toString());
+    }
+}
---
+++ src/main/java/restorearray/Solution.java
@@ -0,0 +1,8 @@
+package restorearray;
+
+public class Solution {
+    public int[] restoreArray(int[][] adjacentPairs) {
+        int[] res = new int[]{};
+        return res;
+    }
+}
