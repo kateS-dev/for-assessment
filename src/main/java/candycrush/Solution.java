ae981d91469fb91491594244eef2cebe25d54ff8
Kate <192233029+kateS-dev@users.noreply.github.com>
2026-03-08 16:05:48 -0400

    chore: working on candy crush

---
+++ src/main/java/candycrush/Solution.java
@@ -0,0 +1,13 @@
+package candycrush;
+
+class Solution {
+    public int[][] candyCrush(int[][] board) {
+        // 1. Scan the board to find the candies that need to be crushed
+        // 2. Mark the candies that need to be crushed
+        // 3. Crush the candies
+        // 4. Gravity the board
+        // 5. Repeat the process until there are no more candies to crush
+
+        return new int[][]{};
+    }
+}
